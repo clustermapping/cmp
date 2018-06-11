15a9bb34d30974c7870a6ac3ba1b90910b10cda1
Anil Kommareddi <kommareddi@gmail.com>
2018-06-11 02:09:32 +0000

    changed screenshots functionality to use puppeteer instead of phantomjs

--- data/web/toImage.js
+++ data/web/toImage.js
@@ -1,33 +1,36 @@
-/* global phantom */
-"use strict";
-var page = require('webpage').create(),
-    system = require('system'),
-    address, output, options;
+const puppeteer = require('puppeteer');
 
-address = system.args[1];
-output = system.args[2];
-options = system.args[3];
-page.viewportSize = { width: 1280, height: 1080 };
-page.open(address, function (status) {
-  if (status !== 'success') {
-    console.log('Unable to load the address!');
-    phantom.exit();
-  } else {
-    if (options == "sparkline") {
-      page.viewportSize = { width: 480, height: 320 };
-    }
-    if (options == "noheader") {
-      page.evaluate(function() {
-        jQuery('footer, header, nav').hide();
+const args = process.argv;
+address = args[2];
+output = args[3];
+options = args[4];
+
+console.log(address + ' '+ output+' '+options);
+
+viewportSize = { width: 1280, height: 1080 };
+if (options == "sparkline") {
+	viewportSize = { width: 480, height: 320 };
+}
+
+(async ()  => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  await page.setViewport(viewportSize);
+  if (options == "noheader") {
+    try {
+      await page.evaluate(function() {
+	    jQuery('footer, header, nav').hide();
         jQuery('body').css('padding-top', 0);
       });
-    }
-    page.evaluate(function() {
-      document.body.bgColor = 'white';
-    });
-    window.setTimeout(function () {
-      page.render(output);
-      phantom.exit();
-    }, 750);
+    } catch(e){}
   }
-});
+  await page.evaluate(function() {
+    document.body.bgColor = 'white';
+  });
+
+  await page.goto(address, {"waitUntil" : "networkidle0"});
+  await page.screenshot({path: output});
+
+  await browser.close();
+})();
